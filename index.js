ed34a03ad2d0e869c862e3314ccdb468aab0c4d5
Aarcoder1406 <110612746+Aarcoder1406@users.noreply.github.com>
2022-08-31 20:21:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let screen= document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for(item of buttons){
+    item.addEventListener('click',(e)=>{
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText)
+        if(buttonText=='x'){
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if(buttonText == 'C'){
+            screenValue = "";
+            screen.value = screenValue;
+
+        }
+        else if(buttonText == '='){
+            screen.value = eval(screenValue);
+
+        }
+        else if(buttonText == 'DEL'){
+            screen.value = screen.value.slice(0,-1);
+        }
+        else{
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+    })
+}
